92665690a7c06cf000db209205a38adbddaba1dd
ShotaSaitoAirport <shota.saito@jalinfotec.co.jp>
2018-12-14 12:36:17 +0900

    空のクラス、アクティビティ作成　斎藤

--- app/src/main/java/com/jalinfotec/kankoannai/AirportGuideDetailActivity.kt
+++ app/src/main/java/com/jalinfotec/kankoannai/AirportGuideDetailActivity.kt
@@ -3,10 +3,10 @@ package com.jalinfotec.kankoannai
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class   AirportGuideActivity : AppCompatActivity() {
+class AirportGuideDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_airport_guide)
+        setContentView(R.layout.activity_airport_guide_detail)
     }
 }
