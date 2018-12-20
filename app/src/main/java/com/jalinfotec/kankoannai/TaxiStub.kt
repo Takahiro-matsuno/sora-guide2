3b999adcb85603e931a4964ac59b8eadbca714ad
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2018-12-20 16:27:42 +0900

    [update]TaxiStubにタクシー予約情報一覧取得を追加。

--- app/src/main/java/com/jalinfotec/kankoannai/TaxiStub.kt
+++ app/src/main/java/com/jalinfotec/kankoannai/TaxiStub.kt
@@ -44,4 +44,68 @@ class TaxiStub {
         } else "予約に失敗しました"
 
     }
+    fun getTaxiBookingInfo(success: Boolean): String? {
+
+        return if (success) {
+
+            "[" +
+                "{" +
+                    "\"bookingId\":\"00001\"," +
+                    "\"bookingStatus\":\"予約中\"," +
+                    "\"rideOnData\":\"2010-12-01\"," +
+                    "\"rideOnTime\":\"12:00:00\"," +
+                    "\"adultPassengerNumber\":2," +
+                    "\"childPassengerNumber\":3," +
+                    "\"taxiNumber\":2," +
+                    "\"companyId\":\"1110\"," +
+                    "\"destination\":\"お台場\"," +
+                    "\"name\":\"ほげほげ\"," +
+                    "\"phonetic\":\"ほげほげ\"," +
+                    "\"userPhoneNumber\":\"09065733926\"," +
+                    "\"mailAddress\":\"ほげほげ\"," +
+                    "\"userComment\":\"ほげほげほげ\"," +
+                    "\"carNumber\":\"ほげほげほげ\"," +
+                    "\"companyPhoneNumber\":\"ほげほげほげほげ\"," +
+                    "\"taxiNotice\":\"ほげー\"" +
+                "}," +
+                "{" +
+                    "\"bookingId\":\"00002\"," +
+                    "\"bookingStatus\":\"編集中\"," +
+                    "\"rideOnData\":\"2010-12-01\"," +
+                    "\"rideOnTime\":\"12:00:00\"," +
+                    "\"adultPassengerNumber\":2," +
+                    "\"childPassengerNumber\":3," +
+                    "\"taxiNumber\":2," +
+                    "\"companyId\":\"1110\"," +
+                    "\"destination\":\"お台場\"," +
+                    "\"name\":\"ぴよぴよ\"," +
+                    "\"phonetic\":\"ほげほげ\"," +
+                    "\"userPhoneNumber\":\"09065733926\"," +
+                    "\"mailAddress\":\"ほげほげ\"," +
+                    "\"userComment\":\"ほげほげほげ\"," +
+                    "\"carNumber\":\"ほげほげほげ\"," +
+                    "\"companyPhoneNumber\":\"ほげほげほげほげ\"," +
+                    "\"taxiNotice\":\"ほげー\"" +
+                "}," +
+                "{" +
+                    "\"bookingId\":\"00003\"," +
+                    "\"bookingStatus\":\"キャンセル済み\"," +
+                    "\"rideOnData\":\"2010-12-01\"," +
+                    "\"rideOnTime\":\"12:00:00\"," +
+                    "\"adultPassengerNumber\":2," +
+                    "\"childPassengerNumber\":3," +
+                    "\"taxiNumber\":2," +
+                    "\"companyId\":\"1110\"," +
+                    "\"destination\":\"お台場\"," +
+                    "\"name\":\"ふがふが\"," +
+                    "\"phonetic\":\"ほげほげ\"," +
+                    "\"userPhoneNumber\":\"09065733926\"," +
+                    "\"mailAddress\":\"ほげほげ\"," +
+                    "\"userComment\":\"ほげほげほげ\"," +
+                    "\"carNumber\":\"ほげほげほげ\"," +
+                    "\"companyPhoneNumber\":\"ほげほげほげほげ\"," +
+                "}," +
+            "]"
+        } else null
+    }
 }
