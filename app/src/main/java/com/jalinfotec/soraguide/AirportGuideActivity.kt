2b85421c896a6f30e720b22d3bdd0685c24cf9b3
ShotaSaitoAirport <shota.saito@jalinfotec.co.jp>
2018-12-20 15:52:09 +0900

    空港施設案内画面を修正　斎藤

--- app/src/main/java/com/jalinfotec/soraguide/AirportGuideActivity.kt
+++ app/src/main/java/com/jalinfotec/soraguide/AirportGuideActivity.kt
@@ -2,11 +2,24 @@ package com.jalinfotec.soraguide
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_airport_guide.*
+import android.widget.ListView
+import java.io.StringBufferInputStream
+
 
 class   AirportGuideActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_airport_guide)
+
+
+        val items = Array(5,{i -> "お店$i"})
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
+        listView.adapter = adapter
+
+
+
     }
 }
