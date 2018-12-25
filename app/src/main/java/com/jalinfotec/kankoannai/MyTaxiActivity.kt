1d8d99c03f090dc3ed2ff080a0123e474ad5c8e9
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2018-12-25 17:56:46 +0900

    [update]MyTaxiActivityにタブを実装

--- app/src/main/java/com/jalinfotec/kankoannai/MyTaxiActivity.kt
+++ app/src/main/java/com/jalinfotec/kankoannai/MyTaxiActivity.kt
@@ -1,10 +1,9 @@
 package com.jalinfotec.kankoannai
 
-import android.app.LauncherActivity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import kotlinx.android.synthetic.main.activity_my_taxi.*
-import kotlinx.android.synthetic.main.activity_my_taxi.view.*
 import org.jetbrains.anko.startActivity
 
 class MyTaxiActivity : AppCompatActivity() {
@@ -15,11 +14,36 @@ class MyTaxiActivity : AppCompatActivity() {
         // アニメーションの無効化
         overridePendingTransition(0, 0)
 
-        reg_tab.setOnClickListener {
-            // タクシー予約画面へ遷移
-            startActivity<TaxiReservationActivity>(
-                Pair(TaxiReservationActivity.fromTaxiKey, TaxiReservationActivity.fromTaxi))
-            this.finish()
-        }
+
+
+        // 指定したタブを選択状態にする
+        tabLayout.getTabAt(1)!!.select()
+        // タブのタップイベント
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            // 選択中のタブがユーザーによって再度選択されたときに呼び出される
+            override fun onTabReselected(p0: TabLayout.Tab?) {}
+
+            // タブが選択された状態を終了したときに呼び出される
+            override fun onTabSelected(p0: TabLayout.Tab?) {}
+
+            // タブが選択状態になると呼び出される
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+                when (tabLayout.selectedTabPosition) {
+                    0 -> {
+                        startActivity<TaxiReservationActivity>(
+                            Pair(TaxiReservationActivity.fromTaxiKey, TaxiReservationActivity.fromTaxi)
+                        )
+                    }
+                    1 -> {
+                        // TODO 確認画面表示中に予約確認をタップした場合、画面をクリアするか確認する
+                        startActivity<MyTaxiActivity>(
+                            Pair(TaxiReservationActivity.fromTaxiKey, TaxiReservationActivity.fromTaxi)
+                        )
+                    }
+                }
+                finish()
+            }
+        })
+
     }
 }
