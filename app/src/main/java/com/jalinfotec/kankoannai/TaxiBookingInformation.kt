f13c37dbc2370a95fea215eceaa4ab4dc8a25cff
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2018-12-28 17:00:09 +0900

    [add]TaxiReservationCheckActivityに予約登録のPOST処理を実装 [add]非同期通信クラス FuelTaskLoaderを作成

--- app/src/main/java/com/jalinfotec/kankoannai/TaxiBookingInformation.kt
+++ app/src/main/java/com/jalinfotec/kankoannai/TaxiBookingInformation.kt
@@ -6,14 +6,14 @@ import java.sql.Time
 class TaxiBookingInformation(
     val bookingId: String?,             // 予約番号
     val bookingStatus: Int,             // ステータス
-    val rideOnData: Date,               // 乗車日付
+    val rideOnDate: Date,               // 乗車日付
     val rideOnTime: Time,               // 乗車時間
     val adultPassengerNumber: Int,      // 人数（大人）
     val childPassengerNumber: Int,      // 人数（子供）
     val taxiNumber: Int,                // 配車台数
     val companyId: String,              // 会社ID
     val destination: String?,           // 目的地
-    val name: String,                   // 指名
+    val name: String,                   // 氏名
     val phonetic: String,               // フリガナ
     val userPhoneNumber: String,        // 電話番号
     val mailAddress: String,            // メールアドレス
