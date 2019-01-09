a995b6a0bc011e1e22049c52c2f160234e4825c4
Yuuya Toyoda <yuuya.s.toyoda@jalinfotec.co.jp>
2019-01-09 14:44:41 +0900

    予約内容詳細画面に全画面から受けとった予約番号を元に予約検索を行い、表示させる処理を追加。 予約内容詳細画面の表示項目を追加。 予約内容詳細画面に表示させる予約データ用のスタブデータを作成。

--- app/src/main/java/com/jalinfotec/kankoannai/MyTaxiDetailActivity.kt
+++ app/src/main/java/com/jalinfotec/kankoannai/MyTaxiDetailActivity.kt
@@ -3,22 +3,41 @@ package com.jalinfotec.kankoannai
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_my_taxi_detail.*
 import org.jetbrains.anko.startActivity
 import java.text.SimpleDateFormat
-import java.util.*
 
 class MyTaxiDetailActivity : AppCompatActivity() {
 
-    private var toast: Toast? = null
-
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_taxi_detail)
 
         //予約登録画面からの情報を表示
         bookingId.text = intent.getStringExtra("ID")
+
+        val bookingDetailInfo = TaxiStub().getTaxiBookingInfo(bookingId.text.toString())
+
+        if(bookingDetailInfo != null) {
+            val df = SimpleDateFormat("yyyy/MM/dd")
+            val tf = SimpleDateFormat("hh:mm")
+            day.text = df.format(bookingDetailInfo.rideOnDate)
+            time.text =  tf.format(bookingDetailInfo.rideOnDate)
+            adlt.text = bookingDetailInfo.adultPassengerNumber.toString()
+            chld.text = bookingDetailInfo.childPassengerNumber.toString()
+            disp.text = bookingDetailInfo.taxiNumber.toString()
+            taxi.text = bookingDetailInfo.companyId
+            dest.text = bookingDetailInfo.destination
+            name.text = bookingDetailInfo.name
+            kana.text = bookingDetailInfo.phonetic
+            phone.text = bookingDetailInfo.userPhoneNumber
+            mail.text = bookingDetailInfo.mailAddress
+            comment.text = bookingDetailInfo.userComment
+            carNumber.text = bookingDetailInfo.carNumber
+            companyPhoneNumber.text = bookingDetailInfo.companyPhoneNumber
+            taxiNotice.text = bookingDetailInfo.taxiNotice
+        }
         /*
         day.text = intent.getStringExtra("DAY")
         time.text = intent.getStringExtra("TIME")
@@ -48,75 +67,6 @@ class MyTaxiDetailActivity : AppCompatActivity() {
         cancelButton.setOnClickListener { postBookingInfo() }
         */
 
-    }
-
-    // 画面がActiveでなくなった時
-    override fun onPause() {
-        super.onPause()
-        toast?.cancel() // 表示中のトーストを消す
-    }
-
-    @SuppressLint("SimpleDateFormat")
-    private fun postBookingInfo() {
-        val sdf = SimpleDateFormat("yyyy/MM/dd hh:mm")
-        val date = sdf.parse("${day.text} ${time.text}")
-        // 予約情報クラスを作成
-        // TODO 未入力箇所はブランクで送信しているためDBの仕様が決まり次第変更する
-        val bookingInfo = TaxiBookingInformation(
-            null,
-            0,
-            Date(date.time),
-            //Time(date.time),
-            adlt.text.toString().toInt(),
-            chld.text.toString().toInt(),
-            disp.text.toString().toInt(),
-            "",
-            dest.text.toString(),
-            name.text.toString(),
-            kana.text.toString(),
-            phone.text.toString(),
-            mail.text.toString(),
-            comment.text.toString(),
-            null,
-            null,
-            null
-        )
-        /*
-        val endpoint = ""
-        val header = null
-        val param = null
-        val body = Gson().toJson(bookingInfo)
-        // TODO API 28から非推奨になったため、いずれ置き換える
-        supportLoaderManager.initLoader(0, null, object : LoaderManager.LoaderCallbacks<String> {
-            override fun onCreateLoader(p0: Int, p1: Bundle?): Loader<String> {
-                return FuelTaskLoader(
-                    this@TaxiReservationCheckActivity,
-                    FuelTaskLoader.FuelManager.Method.POST,
-                    endpoint,
-                    header,
-                    param,
-                    body)
-            }
-            override fun onLoadFinished(loader: Loader<String>, result: String?) {
-                supportLoaderManager.destroyLoader(loader.id)
-                if (result == null) {
-                } else {
-                    // 送信成功の場合は予約完了画面に遷移する
-                    startActivity<TaxiReservationCompActivity>(Pair(TaxiReservationCompActivity.resultKey, result))
-                    this@TaxiReservationCheckActivity.finish()
-                }
-            }
-            override fun onLoaderReset(p0: Loader<String>) {}
-        })
-        */
-        // サーバサイドが未実装のため以下で代用
-        /*
-        val result = TaxiStub().postTaxiBookingInformation(true, bookingInfo)
-        toast =
-                if (result == null) Toast.makeText(this, "エラー", Toast.LENGTH_SHORT)
-                else Toast.makeText(this, result, Toast.LENGTH_SHORT)
-        toast!!.show()
-         */
     }
 
     //戻るボタン制御
--- app/src/main/java/com/jalinfotec/kankoannai/TaxiStub.kt
+++ app/src/main/java/com/jalinfotec/kankoannai/TaxiStub.kt
@@ -1,8 +1,13 @@
 package com.jalinfotec.kankoannai
 
+import android.annotation.SuppressLint
 import android.util.Log
 import com.google.gson.Gson
+import com.jalinfotec.kankoannai.R.string.bookingId
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class TaxiStub {
 
@@ -32,6 +37,7 @@ class TaxiStub {
                     "]"
         } else null
     }
+
     fun postTaxiBookingInformation(success: Boolean, bookingInfo: TaxiBookingInformation): String? {
         return if (success) {
             try {
@@ -44,6 +50,7 @@ class TaxiStub {
             }
         } else null
     }
+
     fun getTaxiBookingInfo(success: Boolean): String? {
 
         return if (success) {
@@ -105,7 +112,42 @@ class TaxiStub {
                     "\"carNumber\":\"ほげほげほげ\"," +
                     "\"companyPhoneNumber\":\"ほげほげほげほげ\"" +
                     "}" +
-            "]"
+                    "]"
         } else null
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun getTaxiBookingInfo(id: String): TaxiBookingInformation? {
+        val bookingInfoList = ArrayList<TaxiBookingInformation>()
+
+        for (count in 0..2) {
+            val bookingInfo = TaxiBookingInformation(
+                "0000" + (count+1).toString(),
+                0,
+                SimpleDateFormat("yyyy/MM/dd hh:mm").parse("2018/11/12 5:00"),
+                count+1,
+                1,
+                1,
+                "1000",
+                "お台場",
+                "なまえ",
+                "ほげほげ",
+                "ほげほげ",
+                "めあど",
+                "りようこめ",
+                "しゃりょう番号",
+                "れんらくさき",
+                "ごあんないじこう"
+            )
+            bookingInfoList.add(bookingInfo)
+        }
+
+        for (count in 0..bookingInfoList.lastIndex) {
+            if (id == bookingInfoList[count].bookingId) {
+                return bookingInfoList[count]
+            }
+        }
+
+        return null
     }
 }
