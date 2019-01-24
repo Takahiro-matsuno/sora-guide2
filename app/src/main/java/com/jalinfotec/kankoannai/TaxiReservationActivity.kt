44b1d8c0ca097d945ffa425541d82f003527f7ca
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-01-24 17:16:13 +0900

    webViewを実装

---
+++ app/src/main/java/com/jalinfotec/kankoannai/TaxiReservationActivity.kt
@@ -0,0 +1,38 @@
+package com.jalinfotec.kankoannai
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import org.jetbrains.anko.find
+
+class TaxiReservationActivity : AppCompatActivity() {
+
+    lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_taxi_reservation)
+
+        // テスト
+        val url = "https://taxiapptest.azurewebsites.net/taxiReservation-0.1.2/"
+        // 登録画面
+        //val url = ""
+        // TODO その他適宜追加
+        //val url = ""
+
+        webView = find(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl(url)
+    }
+    // Android標準ボタンの制御
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        // 戻るボタンを押された場合
+        return if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            // ブラウザバックができる場合はブラウザバックする
+            webView.goBack()
+            true
+        } else super.onKeyDown(keyCode, event) // ブラウザバックできない場合はActivityしTopMenuへ戻る
+    }
+}
--- app/src/main/java/com/jalinfotec/kankoannai/TopMenuActivity.kt
+++ app/src/main/java/com/jalinfotec/kankoannai/TopMenuActivity.kt
@@ -28,10 +28,10 @@ class TopMenuActivity : AppCompatActivity() {
         }
 
         //タクシー予約画面へ遷移
-        val toTaxyButton = findViewById<Button>(R.id.taxi)
-        toTaxyButton.setOnClickListener{
+        val toTaxiButton = findViewById<Button>(R.id.taxi)
+        toTaxiButton.setOnClickListener{
 
-            startActivity<AirportGuideActivity>()
+            startActivity<TaxiReservationActivity>()
         }
 
         //観光案内画面へ遷移
