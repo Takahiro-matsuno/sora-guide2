1cc51868d5f750397d8236df530995bd41f76d48
Yuuya Toyoda <yuuya.s.toyoda@jalinfotec.co.jp>
2019-02-18 14:10:07 +0900

    WebViewのCookieとJSの有効化、アプリ独自のUserAgent設定

--- app/src/main/java/com/jalinfotec/kankoannai/TaxiReservationActivity.kt
+++ app/src/main/java/com/jalinfotec/kankoannai/TaxiReservationActivity.kt
@@ -1,8 +1,13 @@
 package com.jalinfotec.kankoannai
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
+import android.webkit.CookieManager
+import android.webkit.CookieSyncManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import org.jetbrains.anko.find
@@ -10,22 +15,41 @@ import org.jetbrains.anko.find
 class TaxiReservationActivity : AppCompatActivity() {
 
     lateinit var webView: WebView
+    private lateinit var cookieManager: CookieManager
+    private lateinit var cookieSyncManager: CookieSyncManager
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_taxi_reservation)
 
         // テスト
-        val url = "https://taxiapptest.azurewebsites.net/taxiReservation-0.1.2/"
+        //val url = "https://taxiapptest.azurewebsites.net/taxiReservation-0.1.2/"
         // 登録画面
         //val url = "https://taxiapptest.azurewebsites.net/taxiReservation-0.1.2/app/registration"
         // TODO その他適宜追加
-        //val url = ""
+        //localhostで実行しているWebアプリケーション
+        val url = "http://10.0.2.2:8080"
 
+        //Cookie有効化
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            CookieSyncManager.createInstance(applicationContext)
+        }
+        cookieManager = CookieManager.getInstance()
+        cookieManager.setAcceptCookie(true)
+
+        //WebView設定
         webView = find(R.id.webView)
         webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        //UserAgent設定
+        val userAgent = webView.settings.userAgentString
+        webView.settings.userAgentString = userAgent + "sora-GuideApp"
+
         webView.loadUrl(url)
     }
+
     // Android標準ボタンの制御
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
         // 戻るボタンを押された場合
@@ -35,4 +59,25 @@ class TaxiReservationActivity : AppCompatActivity() {
             true
         } else super.onKeyDown(keyCode, event) // ブラウザバックできない場合はActivityしTopMenuへ戻る
     }
+
+    override fun onPause() {
+        super.onPause()
+        webView.pauseTimers()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.flush()
+        } else {
+            CookieSyncManager.getInstance().stopSync()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        webView.resumeTimers()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.flush()
+        } else {
+            CookieSyncManager.getInstance().startSync()
+        }
+    }
+
 }
