40a6ca41bd2f417afaf23f5530028ca17051dce7
Yuuya Toyoda <yuuya.s.toyoda@jalinfotec.co.jp>
2019-02-18 14:10:48 +0900

    WebViewのCookieとJSの有効化、アプリ独自のUserAgent設定

--- app/src/main/java/com/jalinfotec/kankoannai/TaxiReservationActivity.kt
+++ app/src/main/java/com/jalinfotec/kankoannai/TaxiReservationActivity.kt
@@ -1,10 +1,9 @@
 package com.jalinfotec.kankoannai
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.KeyEvent
 import android.webkit.CookieManager
 import android.webkit.CookieSyncManager
